eb80db8f0458150f0200acf851bc794fd6609050
clr-cera <clrcera05@gmail.com>
2025-03-19 14:59:45 -0300

    Advances on Yolov7 load

--- src/model.py
+++ src/model.py
@@ -1,13 +1,18 @@
-import wget, os
+import wget, os, sys
 import gdown
 import torch
 from git import Repo
 
 def download_models():
+    ensure_dir('models/')
+    
     # Yolov7.pt
-    download_wget('https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7.pt','models/yolov7.pt')
-    Repo.clone_from('https://github.com/WongKinYiu/yolov7.git', 'models/yolov7/')
+    ensure_dir('models/objects/')
 
+    download_wget('https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7.pt','models/objects/yolov7.pt')
+    if not os.path.exists('models/objects/yolov7'):
+        Repo.clone_from('https://github.com/WongKinYiu/yolov7.git', 'models/objects/yolov7/')
+    download_gdown('1SAUaYzNJdQeZ2r1gmf6tLFB6oRclB4RA', 'models/objects/coco_categories.csv')
 
     # Whole_Wideresnet18_places365.pth
     download_gdown('1D6bGoJHuzXJhnr5KI70Zj1PkbGWWtWij', 'models/scenes/')
@@ -18,6 +23,9 @@ def download_models():
     # Model_Age
     download_gdown('1ZF33ousEHhAwK8MmNXpuwmvVtXilVAJ_', 'models/model_age/')
 
+def ensure_dir(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
 
 def download_wget(url: str, out: str):
     if not os.path.exists(out):
@@ -28,8 +36,10 @@ def download_gdown(id: str, out: str):
         gdown.download(id=id, output=out)
 
 def infer_objects(dataset: str, device: torch.device):
-    path = 'models/yolov7.pt'
-    model = torch.hub.load("WongKinYiu/yolov7", "custom", path, trust_repo=True)
-    
-    results = model("sentiment-dataset/images/0a4d4c7d-e520-4c11-b969-71ceee1b40d0.jpg")
-    print(results)
+    # I'm sorry for the crimes I am about to commit
+    sys.path.append('./models/yolov7')
+    from models.experimental import attempt_load # type: ignore
+    from utils.general import check_img_size, non_max_suppression, scale_coords # type: ignore
+    from utils.datasets import LoadImages # type: ignore
+    from utils.torch_utils import select_device, TracedModel # type: ignore
+
