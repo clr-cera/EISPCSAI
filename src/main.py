18e17678459c7bb67bf9c95209541f920032de67
clr-cera <clrcera05@gmail.com>
2025-03-25 11:29:40 -0300

    update, now focusing on age gender model

--- src/main.py
+++ src/main.py
@@ -16,6 +16,7 @@ parser.description='Attacking CSAI detection with Proxy Tasks ensembling'
 # Dataset manipulation
 parser.add_argument("--download_dataset", help='This is the dataset to be downloaded', dest='download_dataset_name', type=str, default=None)
 parser.add_argument("--download_models", help="This is a boolean value to download all models", dest="download_models", action='store_true')
+parser.add_argument("--test_object", help="When this option is set the object detection model will be tested in an image from the sentiment-dataset", dest="test_object", action='store_true')
 
 args = parser.parse_args()
 
@@ -25,4 +26,8 @@ if args.download_dataset_name:
 if args.download_models:
     model.download_models()
 
-#model.infer_objects(dataset="sentiment-dataset", device=device)
+
+#model.infer_objects(dataset="sentiment-dataset", device=device, test=args.test_object)
+#model.infer_nsfw(dataset="sentiment-dataset", device=device, test=True)
+#model.infer_scenes(dataset='sentiment-dataset', device=device, test=True)
+model.infer_age_gender(dataset='sentiment-dataset', device=device, test=True)
--- src/model.py
+++ src/model.py
@@ -1,29 +1,35 @@
-import wget, os, sys
-import gdown
-import torch
-from git import Repo
+import os
+
+import gdown, wget, torch, torchvision, cv2
+from torchvision import transforms as trn
+from torch.nn import functional as F
+from torch.autograd import Variable as V
+import transformers
+from PIL import Image
+from ultralytics import YOLO
+
+TEST_PATH = "sentiment-dataset/images/0d8a7158-b805-4ce5-967b-7ec631547e71.jpg"
 
 def download_models():
     ensure_dir('models/')
     
-    # Yolov7.pt
+    # Yolov11.pt
     ensure_dir('models/objects/')
+    download_wget('https://github.com/ultralytics/assets/releases/download/v8.3.0/yolo11x.pt','models/objects/yolov11.pt')
 
-    download_wget('https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7.pt','models/objects/yolov7.pt')
-    if not os.path.exists('models/objects/yolov7'):
-        Repo.clone_from('https://github.com/WongKinYiu/yolov7.git', 'models/objects/yolov7/')
-    download_gdown('1SAUaYzNJdQeZ2r1gmf6tLFB6oRclB4RA', 'models/objects/coco_categories.csv')
-
-    # Whole_Wideresnet18_places365.pth
-    download_gdown('1D6bGoJHuzXJhnr5KI70Zj1PkbGWWtWij', 'models/scenes/')
-    # Nsfw_mobilenet2.224x224.h5
-    download_gdown('1t8cAnS8rNBQU8vo16CDAiBL0RuTJdesi', 'models/nsfw_model/')
+    # Places365
+    download_wget('http://places2.csail.mit.edu/models_places365/alexnet_places365.pth.tar','models/scenes/alexnet_places365.pth.tar')
     # Shape_Predictor_68_face_landmarks.dat
     download_gdown('1AEtQ2s4k5R7IKdrK6vs_zqH_DvXIeFUK', 'models/fitzpatrick/')
+
     # Model_Age
     download_gdown('1ZF33ousEHhAwK8MmNXpuwmvVtXilVAJ_', 'models/model_age/')
 
-def ensure_dir(path):
+    # Nsfw_mobilenet2.224x224.h5
+    #download_gdown('1t8cAnS8rNBQU8vo16CDAiBL0RuTJdesi', 'models/nsfw_model/')
+    
+
+def ensure_dir(path: str):
     if not os.path.exists(path):
         os.mkdir(path)
 
@@ -35,11 +41,82 @@ def download_gdown(id: str, out: str):
     if not os.path.exists(out):
         gdown.download(id=id, output=out)
 
-def infer_objects(dataset: str, device: torch.device):
-    # I'm sorry for the crimes I am about to commit
-    sys.path.append('./models/yolov7')
-    from models.experimental import attempt_load # type: ignore
-    from utils.general import check_img_size, non_max_suppression, scale_coords # type: ignore
-    from utils.datasets import LoadImages # type: ignore
-    from utils.torch_utils import select_device, TracedModel # type: ignore
+def infer_objects(dataset: str, device: torch.device, test: bool = False):
+    model = YOLO('models/objects/yolov11.pt')
+   
+    if test:
+        
+        results = model(TEST_PATH)
+        # Access the results
+        for result in results: 
+            xyxy = result.boxes.xyxy  # top-left-x, top-left-y, bottom-right-x, bottom-right-y
+            names = [result.names[cls.item()] for cls in result.boxes.cls.int()]  # class name of each box
 
+            image = cv2.imread(TEST_PATH)
+            color = (256, 0, 0)
+            thickness = 2
+            for box in xyxy:
+                start_point = (int(box[0].item()), int(box[1].item()))
+                end_point = (int(box[2].item()), int(box[3].item()))
+                image = cv2.rectangle(image, start_point, end_point, color, thickness)
+
+            cv2.imwrite('test.jpg', image)
+            print(names)
+
+def infer_nsfw(dataset: str, device: torch.device, test: bool = False):
+    predict = transformers.pipeline("image-classification", model="AdamCodd/vit-base-nsfw-detector",device=device, use_fast=True)
+
+    img = Image.open(TEST_PATH)
+    results = predict(img)
+    print(results)
+
+def infer_scenes(dataset: str, device: torch.device, test: bool = False):
+        
+    # th architecture to use
+    arch = 'alexnet'
+
+    # load the pre-trained weights
+    model_file = 'models/scenes/alexnet_places365.pth.tar'
+
+    model = torchvision.models.__dict__[arch](num_classes=365)
+    checkpoint = torch.load(model_file, map_location=lambda storage, loc: storage)
+    state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
+    model.load_state_dict(state_dict)
+    model.eval()
+
+
+    # load the image transformer
+    centre_crop = trn.Compose([
+            trn.Resize((256,256)),
+            trn.CenterCrop(224),
+            trn.ToTensor(),
+            trn.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    # load the class label
+    file_name = 'models/scenes/categories_places365.txt'
+    classes = list()
+    with open(file_name) as class_file:
+        for line in class_file:
+            classes.append(line.strip().split(' ')[0][3:])
+    classes = tuple(classes)
+
+    if test:
+        # load the test image
+        img_name = TEST_PATH
+
+        img = Image.open(img_name)
+        input_img = V(centre_crop(img).unsqueeze(0))
+
+        # forward pass
+        logit = model.forward(input_img)
+        h_x = F.softmax(logit, 1).data.squeeze()
+        probs, idx = h_x.sort(0, True)
+
+        print('{} prediction on {}'.format(arch,img_name))
+        # output the prediction
+        for i in range(0, 5):
+            print('{:.3f} -> {}'.format(probs[i], classes[idx[i]]))
+
+def infer_age_gender(dataset: str, device: torch.device, test: bool = False):
+    pass
