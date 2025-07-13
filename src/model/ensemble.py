0eefe5e6c94e9366f622c4f1dab86bcdf575a32e
clr <clrcera05@gmail.com>
2025-07-13 15:43:21 -0300

    saving data

--- src/model/ensemble.py
+++ src/model/ensemble.py
@@ -10,8 +10,12 @@ import shap
 
 
 def train_ensemble_sentiment(path_to_features, path_to_labels):
-    train_first_question(path_to_features, path_to_labels)
-    train_second_question(path_to_features, path_to_labels)
+    data = [
+        train_first_question(path_to_features, path_to_labels),
+        train_second_question(path_to_features, path_to_labels),
+    ]
+
+    save_data(data, "resultsq1.csv")
 
 
 def train_first_question(path_to_features, path_to_labels):
@@ -61,14 +65,18 @@ def train_first_question(path_to_features, path_to_labels):
     predicted_classes = predictions.argmax(axis=1)
 
     acc = accuracy_score(y_test, predicted_classes)
+    logging.info(f"Accuracy: {acc * 100:.2f}%")
 
     save_confusion_matrix(y_test, predicted_classes, "confusion_matrix_q1.png")
 
     explainer = shap.TreeExplainer(bst)
     shap_values = explainer.shap_values(X_train)
-    save_shap_plot(shap_values, acc, "shap_plot_q1.png")
+    age_gender_shap, ita_shap, objects_shap, nsfw_shap, scene_shap = save_shap_plot(
+        shap_values, acc, "shap_plot_q1.png"
+    )
 
     bst.save_model("models/ensemble/ensemble_sentiment_q1.json")
+    return acc, age_gender_shap, ita_shap, objects_shap, nsfw_shap, scene_shap
 
 
 def train_second_question(path_to_features, path_to_labels):
@@ -118,14 +126,18 @@ def train_second_question(path_to_features, path_to_labels):
     predicted_classes = predictions.argmax(axis=1)
 
     acc = accuracy_score(y_test, predicted_classes)
+    logging.info(f"Accuracy: {acc * 100:.2f}%")
 
     save_confusion_matrix(y_test, predicted_classes, "confusion_matrix_q2.png")
 
     explainer = shap.TreeExplainer(bst)
     shap_values = explainer.shap_values(X_train)
-    save_shap_plot(shap_values, acc, "shap_plot_q2.png")
+    age_gender_shap, ita_shap, objects_shap, nsfw_shap, scene_shap = save_shap_plot(
+        shap_values, acc, "shap_plot_q2.png"
+    )
 
     bst.save_model("models/ensemble/ensemble_sentiment_q2.json")
+    return acc, age_gender_shap, ita_shap, objects_shap, nsfw_shap, scene_shap
 
 
 def save_confusion_matrix(y_test, predicted_classes, img_name="confusion_matrix.png"):
@@ -150,11 +162,11 @@ def save_shap_plot(shap_values, acc, img_name="shap_plot.png"):
     nsfw_shap_mean = np.abs(np.mean(nsfw_shap))
     scene_shap_mean = np.abs(np.mean(scene_shap))
 
-    print("Age Gender importance:", age_gender_shap_mean)
-    print("Ita importance:", ita_shap_mean)
-    print("Objects importance:", objects_shap_mean)
-    print("NSFW importance:", nsfw_shap_mean)
-    print("Scene importance:", scene_shap_mean)
+    logging.info(f"Age Gender importance: {age_gender_shap_mean}")
+    logging.info(f"Ita importance: {ita_shap_mean}")
+    logging.info(f"Objects importance: {objects_shap_mean}")
+    logging.info(f"NSFW importance: {nsfw_shap_mean}")
+    logging.info(f"Scene importance: {scene_shap_mean}")
     plt.bar(
         ["Age Gender", "Ita", "Objects", "NSFW", "Scene"],
         [
@@ -169,6 +181,21 @@ def save_shap_plot(shap_values, acc, img_name="shap_plot.png"):
     plt.savefig(img_name, dpi=300, bbox_inches="tight")
     plt.clf()
     logging.info(f"SHAP plot saved to {img_name}")
+    return (
+        age_gender_shap_mean,
+        ita_shap_mean,
+        objects_shap_mean,
+        nsfw_shap_mean,
+        scene_shap_mean,
+    )
+
+
+def save_data(data, filename):
+    df = pd.DataFrame(
+        data, columns=["Accuracy", "Age Gender", "Ita", "Objects", "NSFW", "Scene"]
+    )
+    df.to_csv(filename, index=True)
+    logging.info(f"Data saved to {filename}")
 
 
 def load_ensemble_model(path_to_model):
