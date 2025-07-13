128bff37cc6407b3398b1786b43d4baaa0926ddd
clr <clrcera05@gmail.com>
2025-07-13 14:55:21 -0300

    starting second question

--- src/model/ensemble.py
+++ src/model/ensemble.py
@@ -11,14 +11,12 @@ import shap
 
 def train_ensemble_sentiment(path_to_features, path_to_labels):
     train_first_question(path_to_features, path_to_labels)
+    train_second_question(path_to_features, path_to_labels)
 
 
 def train_first_question(path_to_features, path_to_labels):
     # Load features and labels
     X = np.load(path_to_features)
-
-    # Define the feature sizes
-    feature_sizes = [4096, 1, 768, 768, 256]
 
     # Average all Q1 labels
     dfy = pd.read_csv(path_to_labels, sep=";")
@@ -26,15 +24,15 @@ def train_first_question(path_to_features, path_to_labels):
     y = dfy.iloc[:, col_idxs].mean(axis=1).values
     y = np.round(y)
 
-    logging.info(f"Labels shape: {y.shape}")
-    logging.info(f"Features shape: {X.shape}")
-
     # 0 indexing
     y = y - 1
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=123
     )
+
+    logging.info(f"Labels shape: {y.shape}")
+    logging.info(f"Features shape: {X.shape}")
     logging.info(f"Train shape: {X_train.shape}, {y_train.shape}")
     logging.info(f"Test shape: {X_test.shape}, {y_test.shape}")
 
@@ -57,8 +55,6 @@ def train_first_question(path_to_features, path_to_labels):
     bst = xgb.train(params, dtrain, num_boost_round=128)
 
     # Uncomment the following lines to load the model from a file
-    # bst = xgb.Booster()
-    # bst.load_model("models/ensemble/ensemble_sentiment.json")
 
     X_test = xgb.DMatrix(X_test)
     predictions = bst.predict(X_test)
@@ -66,15 +62,82 @@ def train_first_question(path_to_features, path_to_labels):
 
     acc = accuracy_score(y_test, predicted_classes)
 
-    logging.info("Accuracy on test data: {:.1f}%".format(acc * 100))
-    cm = confusion_matrix(y_test, predicted_classes)
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-    disp.plot().figure_.savefig("confusion_matrix.png")
-    plt.clf()
+    save_confusion_matrix(y_test, predicted_classes, "confusion_matrix_q1.png")
 
     explainer = shap.TreeExplainer(bst)
     shap_values = explainer.shap_values(X_train)
+    save_shap_plot(shap_values, acc, "shap_plot_q1.png")
 
+    bst.save_model("models/ensemble/ensemble_sentiment_q1.json")
+
+
+def train_second_question(path_to_features, path_to_labels):
+    # Load features and labels
+    X = np.load(path_to_features)
+
+    # Average all Q1 labels
+    dfy = pd.read_csv(path_to_labels, sep=";")
+    col_idxs = [3 + 26 * i for i in range(5)]
+    y = dfy.iloc[:, col_idxs].mean(axis=1).values
+    y = np.round(y)
+
+    # 0 indexing
+    y = y - 1
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=123
+    )
+
+    logging.info(f"Labels shape: {y.shape}")
+    logging.info(f"Features shape: {X.shape}")
+    logging.info(f"Train shape: {X_train.shape}, {y_train.shape}")
+    logging.info(f"Test shape: {X_test.shape}, {y_test.shape}")
+
+    # Convert to DMatrix for XGBoost
+    dtrain = xgb.DMatrix(X_train, label=y_train)
+
+    # Set parameters for XGBoost
+    params = {
+        "tree_method": "hist",
+        "objective": "multi:softprob",
+        "eval_metric": "mlogloss",
+        "num_class": 9,
+        "device": "cuda" if torch.cuda.is_available() else "cpu",
+        "verbosity": 3,
+        "max_depth": 7,
+        "eta": 0.1,
+    }
+
+    # Train the model
+    bst = xgb.train(params, dtrain, num_boost_round=128)
+
+    # Uncomment the following lines to load the model from a file
+
+    X_test = xgb.DMatrix(X_test)
+    predictions = bst.predict(X_test)
+    predicted_classes = predictions.argmax(axis=1)
+
+    acc = accuracy_score(y_test, predicted_classes)
+
+    save_confusion_matrix(y_test, predicted_classes, "confusion_matrix_q2.png")
+
+    explainer = shap.TreeExplainer(bst)
+    shap_values = explainer.shap_values(X_train)
+    save_shap_plot(shap_values, acc, "shap_plot_q2.png")
+
+    bst.save_model("models/ensemble/ensemble_sentiment_q2.json")
+
+
+def save_confusion_matrix(y_test, predicted_classes, img_name="confusion_matrix.png"):
+    cm = confusion_matrix(y_test, predicted_classes)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot().figure_.savefig(img_name)
+    plt.clf()
+
+    logging.info(f"Confusion matrix saved to {img_name}")
+
+
+def save_shap_plot(shap_values, acc, img_name="shap_plot.png"):
     age_gender_shap = np.sum(shap_values[:, :4096], axis=1)
     ita_shap = np.sum(shap_values[:, 4096:4097], axis=1)
     objects_shap = np.sum(shap_values[:, 4097 : 4097 + 768], axis=1)
@@ -103,6 +166,12 @@ def train_first_question(path_to_features, path_to_labels):
         ],
     )
     plt.title(f"SHAP Feature Importance, acc= {acc * 100:.1f}%")
-    plt.savefig("shap_bar_1.png", dpi=300, bbox_inches="tight")
+    plt.savefig(img_name, dpi=300, bbox_inches="tight")
+    plt.clf()
+    logging.info(f"SHAP plot saved to {img_name}")
 
-    bst.save_model("models/ensemble/ensemble_sentiment_1.json")
+
+def load_ensemble_model(path_to_model):
+    bst = xgb.Booster()
+    bst.load_model(path_to_model)
+    return bst
