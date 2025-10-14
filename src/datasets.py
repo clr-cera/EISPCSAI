324490ddfc70408b8941416e1663902783f82c78
clr <clrcera05@gmail.com>
2025-10-14 17:43:31 -0300

    certificate issues and docker compose start at the right directory

--- src/datasets.py
+++ src/datasets.py
@@ -1,13 +1,23 @@
 import wget
 import zipfile
 import os
+import ssl
+
+
+
+# Apparently the dataset server has an invalid SSL certificate
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 # Sentiment analysis
 
+
 def download_dataset(name: str):
-    if name=='sentiment':
-        if not os.path.exists('sentiment-dataset'):
-            wget.download('https://datasets.simula.no/downloads/image-sentiment/sentiment.zip')
-            with zipfile.ZipFile('sentiment.zip', 'r') as zip_ref:
-                zip_ref.extractall('.')
-            os.remove('sentiment.zip')
+    if name == "sentiment":
+        if not os.path.exists("sentiment-dataset"):
+            wget.download(
+                "https://datasets.simula.no/downloads/image-sentiment/sentiment.zip"
+            )
+            with zipfile.ZipFile("sentiment.zip", "r") as zip_ref:
+                zip_ref.extractall(".")
+            os.remove("sentiment.zip")
