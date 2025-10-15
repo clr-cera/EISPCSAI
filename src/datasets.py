eb754b79c039662be1a9213d14af8bbed710c24a
clr <clrcera05@gmail.com>
2025-10-14 21:09:27 -0300

    declare some more fixes

--- src/datasets.py
+++ src/datasets.py
@@ -2,6 +2,7 @@ import wget
 import zipfile
 import os
 import ssl
+import csv
 
 
 
@@ -21,3 +22,19 @@ def download_dataset(name: str):
             with zipfile.ZipFile("sentiment.zip", "r") as zip_ref:
                 zip_ref.extractall(".")
             os.remove("sentiment.zip")
+
+            # This file is corrupted, so we remove it
+            os.remove("sentiment-dataset/images/0c416a95-6e96-4e8c-bab3-56b682feafe9.jpg")
+            csv_file = "sentiment-dataset/annotations.csv"
+            # Remove file from csv
+            rows = []
+            with open(csv_file, "r") as f:
+                reader = csv.reader(f, delimiter=";")
+                for row in reader:
+                    if row[1] != "0c416a95-6e96-4e8c-bab3-56b682feafe9.jpg":
+                        rows.append(row)
+            with open(csv_file, "w", newline="") as f:
+                writer = csv.writer(f, delimiter=";")
+                writer.writerows(rows)
+
+            print("Sentiment dataset downloaded and extracted.")
