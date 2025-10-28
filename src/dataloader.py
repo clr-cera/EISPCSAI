0e51d063ceb8f1024fa30884dce4c86a36f37f98
clr <clrcera05@gmail.com>
2025-10-28 16:07:29 -0300

    rcpddataloader

--- src/dataloader.py
+++ src/dataloader.py
@@ -49,3 +49,32 @@ def test_dataloader():
         print(f"Image shape: {images.shape}")
         print(f"Labels: {labels}")
         break  # Remove this line to iterate through the entire dataset
+
+
+class RCPDDataloader(Dataset):
+    def __init__(self, prepend_img_dir: str, transform=None):
+        self.data = pd.read_csv("rcpd/rcpd_annotation_processed.csv")
+        self.prepend_img_dir = prepend_img_dir
+        if not transform:
+            self.transform = transforms.Compose(
+                [
+                    transforms.Resize((256, 256)),
+                ]
+            )
+        else:
+            self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.prepend_img_dir, self.data.iloc[idx, 2])
+        image = decode_image(img_path)
+        if image.shape[0] == 1:  # If grayscale, convert to RGB
+            image = image.repeat(3, 1, 1)
+        label = self.data.iloc[idx, -1]
+        if image.shape[0] == 4:
+            image = image[:3, :, :]
+        if self.transform:
+            image = self.transform(image)
+        return image, label
