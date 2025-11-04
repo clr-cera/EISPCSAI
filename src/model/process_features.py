d6bc65046a7dbc926d0b6a3a978906fff705db8e
clr <clrcera05@gmail.com>
2025-11-04 11:46:19 -0300

    debug

--- src/model/process_features.py
+++ src/model/process_features.py
@@ -272,6 +272,7 @@ def generate_tsne_per_feature(
     pca_features = load_features(
         "features/pca_features.npy", feature_sizes=[256, 1, 256, 256, 256, 256]
     )
+    print("aaaaa is it here?\n\n\n\n\n")
     feature_names = [
         "agegender",
         "ita",
